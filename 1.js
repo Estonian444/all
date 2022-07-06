b9fc42767014f1e5c33f5019759ef87dec5b3228
Estonian444 <qazwsxed25015@mail.ru>
2022-07-06 23:46:10 +0800

    HW_1

---
+++ 1.js
@@ -0,0 +1,39 @@
+let name = "sergei"
+let name2 = "Sergei"
+
+console.log(name2)
+
+let age = 27
+console.log(age) 
+
+let person = name + " " + age 
+console.log(person) 
+
+let user_age = 30
+let work_period = 10
+
+let total_time = user_age - work_period
+console.log("total_time ="+ total_time, typeof("total_time =" + total_time)) 
+
+
+let  morning = false
+console.log("morning =", morning, typeof(morning))  
+
+let evening = true
+console.log("evening =", evening, typeof(evening)) 
+
+let apples = 15 
+let cherry = 5
+
+let compare = apples >= cherry
+console.log("compare =", compare)
+
+if (compare){
+    console.log("Compere is", compare)
+}else if (apples == 15) {
+    console.log(("apples ==", apples)) 
+} else {
+    console.log("!_Compere is", false)
+}
+
+
